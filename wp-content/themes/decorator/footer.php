4ae4dfffc8574742858e6c45aba2185addcbf184
root <root@vps.server.local>
2020-08-09 00:58:25 +0000

    Migracao site novo servidor

--- wp-content/themes/decorator/footer.php
+++ wp-content/themes/decorator/footer.php
@@ -11,10 +11,16 @@
 
 <div class="copyright-wrapper">
         	<div class="container">
-                <div class="copyright">
+                <div class="copyright top-center">
+					<p>Formas de pagamento:</p>
+						<img style="width: 45%; background-color: navajowhite;" src="miniaturas/paymentBanner.png">
+					<br>
                     	<p><?php esc_attr(bloginfo( 'name' )); ?>  
                       <a href="http://danieltecnologia.com" target="_blank"><p>Desenvolvido por DanielTECH</p></a>
-                </div><!-- copyright --><div class="clear"></div>           
+               
+                    	
+                </div>
+				<!-- copyright --><div class="clear"></div>           
             </div><!-- container -->
         </div>
     </div>
